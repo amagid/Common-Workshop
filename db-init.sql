f6657e6e518e210c7dde2ebda31d6bcef100ada9
Aaron Magid <ahm64@case.edu>
2019-03-28 22:05:52 -0400

    chore(DB): Removed extra database setup in sql files.

--- db-init.sql
+++ db-init.sql
@@ -5,8 +5,8 @@
 -- Create Databases If They Don't Exist
 --
 
-CREATE DATABASE IF NOT EXISTS boilerplate;
-CREATE DATABASE IF NOT EXISTS boilerplate_testing;
+CREATE DATABASE IF NOT EXISTS common_workshop;
+CREATE DATABASE IF NOT EXISTS common_workshop_testing;
 
 --
 -- Create Server User
@@ -15,19 +15,8 @@ CREATE DATABASE IF NOT EXISTS boilerplate_testing;
 FLUSH PRIVILEGES;
 
 -- Create General Server User
-DROP USER IF EXISTS 'boilerplate_server'@'localhost';
-CREATE USER 'boilerplate_server'@'localhost' IDENTIFIED WITH mysql_native_password BY '8237OIULB$@(IEOiuef2';
-GRANT CREATE, INSERT, SELECT, UPDATE, DELETE ON boilerplate.* TO 'boilerplate_server'@'localhost';
-
--- Create Testing DB Import User
-DROP USER IF EXISTS 'importer'@'localhost';
-CREATE USER 'importer'@'localhost' IDENTIFIED WITH mysql_native_password BY 'importer';
-GRANT ALL ON boilerplate_testing TO 'importer'@'localhost';
-GRANT ALL ON boilerplate_testing.* TO 'importer'@'localhost';
-
--- Create Testing DB Server User (separate from import user so that it can have the same permissions as the general server user)
-DROP USER IF EXISTS 'boilerplate_server_testing'@'localhost';
-CREATE USER 'boilerplate_server_testing'@'localhost' IDENTIFIED WITH mysql_native_password BY '*O&hbio4FOSR4o8w4iubs(&H$';
-GRANT CREATE, INSERT, SELECT, UPDATE, DELETE ON boilerplate_testing.* TO 'boilerplate_server_testing'@'localhost';
+DROP USER IF EXISTS 'common_workshop_server'@'localhost';
+CREATE USER 'common_workshop_server'@'localhost' IDENTIFIED WITH mysql_native_password BY '8237OIULB$@(IEOiuef2';
+GRANT CREATE, INSERT, SELECT, UPDATE, DELETE ON common_workshop.* TO 'common_workshop_server'@'localhost';
 
 FLUSH PRIVILEGES;
