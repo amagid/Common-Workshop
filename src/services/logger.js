217409f0d093485035e7ff53c8b0a53cd45821d2
Aaron Magid <ahm64@case.edu>
2019-03-28 22:35:57 -0400

    chore(Setup): Simplified Logger.

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,51 +1,3 @@
-const winston = require('winston');
-require('winston-daily-rotate-file');
-const path = require('path');
-
-const normalTransport = new winston.transports.DailyRotateFile({
-    filename: path.join(__dirname, '../../logs/' + (process.env.APP_TESTING === 'true' ? 'testing/' : ''), '%DATE%.log'),
-    datePattern: 'YYYY-MM-DD',
-    prepend: true,
-    handleExceptions: true,
-    humanReadableUnhandledException: true,
-    json: true
-});
-
-const exceptionTransport = new winston.transports.DailyRotateFile({
-    filename: path.join(__dirname, '../../logs/' + (process.env.APP_TESTING === 'true' ? 'testing/' : '') + 'exceptions/', '%DATE%.exceptions'),
-    datePattern: 'YYYY-MM-DD',
-    prepend: true,
-    handleExceptions: true,
-    humanReadableUnhandledException: true,
-    json: true
-});
-
-// Only output to the console if we are not in testing mode.
-const logger = process.env.APP_TESTING === 'true' ? 
-    new(winston.Logger)({
-        transports: [
-            normalTransport
-        ],
-        exceptionHandlers: [
-            normalTransport,
-            exceptionTransport
-        ],
-        exitOnError: false
-    })
-    :
-    new(winston.Logger)({
-        transports: [
-            normalTransport,
-            new winston.transports.Console()
-        ],
-        exceptionHandlers: [
-            normalTransport,
-            new winston.transports.Console(),
-            exceptionTransport
-        ],
-        exitOnError: false
-    })
-
 function info(data) {
     return log("info", data.message, data.status, data.additionalData, data.stack);
 }
@@ -58,28 +10,14 @@ function error(data) {
     return log("error", data.message, data.status, data.additionalData, data.stack);
 }
 
-function request(data) {
-    return log("info", data.message, data.status, data.additionalData, data.stack);
-}
-
 function log(level, message, status, additionalData, stack) {
-    const messageObject = {};
+    const messageObject = { level, message, status };
 
-    if (status) {
-        messageObject.status = status;
-    }
-    if (additionalData) {
-        messageObject.additionalData = additionalData;
-    }
-    if (stack) {
-        messageObject.stack = stack;
-    }
-    return logger.log(level, message, messageObject);
+    console.log(JSON.stringify(messageObject, null, 4));
 }
 
 module.exports = {
     info,
     warn,
-    error,
-    request
+    error
 };
