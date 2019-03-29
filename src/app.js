1977a020372624483cf3ba1055f43429349f3177
Aaron Magid <ahm64@case.edu>
2019-03-28 22:29:53 -0400

    chore(Setup): Simplified App.js.

--- src/app.js
+++ src/app.js
@@ -24,33 +24,6 @@ const startup = db.connect()
 function startServer() {
     const server = http.Server(app);
 
-    server.on('error', function(err) {
-        //If the address is already in use
-        if (err.code === 'EADDRINUSE') {
-            logger.error(
-                  '\n\n========================================\n'
-                + '\n'
-                + 'ERROR: Address In Use (EADDRINUSE)!\n'
-                + '\n'
-                + 'The server could not start because its\n'
-                + `configured port (Port ${process.env.PORT || config.app.port}) is already\n`
-                + 'in use by another application. This is\n'
-                + 'usually caused by an attempt to start a\n'
-                + 'second instance of this server while\n'
-                + 'another instance is running.\n'
-                + '\n'
-                + 'Check to make sure there isn\'t already\n'
-                + 'another instance of this application\n'
-                + 'running and try again.\n'
-                + '\n'
-                + '=======================================\n\n'
-            );
-            process.exit(0);
-        } else {
-            logger.error(JSON.stringify(err, null, 4));
-        }
-    });
-
     server.listen(process.env.PORT || config.app.port);
     logger.info({ message: `Server listening on port ${process.env.PORT || config.app.port}` });
 
@@ -60,23 +33,15 @@ function startServer() {
 function setUpAPI() {
     const routes = require('./routes');
 
-    //App settings
-    app.set('etag', false);
-    app.set('trust proxy', true);
-
     //General middlewares
-    if (!process.env.APP_TESTING) {
-        app.use(morgan('dev'));
-    }
+    app.use(morgan('dev'));
     app.use(bodyParser.json({
         type: 'application/json'
     }));
-    app.use(bodyParser.raw({
-        type: 'application/octet-stream'
-    }));
     app.use(cors());
     app.use(responsePromise);
-    //Mount routes
+    
+    //Register API Routes
     const router = express.Router();
     routes(router);
     app.use('/', router);
