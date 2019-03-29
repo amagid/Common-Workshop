9299a96e76041109fc49291402d34f60e519bfc2
Aaron Magid <ahm64@case.edu>
2019-03-28 22:04:06 -0400

    feat(Config): Simplified configuration system.

--- config/index.js
+++ config/index.js
@@ -1,26 +1,13 @@
 'use strict';
-const dev = require('./development');
-const prod = require('./production');
-const test = require('./testing');
-dev.regexes = require('./regexes-dev');
-prod.regexes = require('./regexes-prod');
+const config = require('./configuration');
 
 /**
- * Gets the relevant configuration values for the current environment. The
- * current environment is specified by the APP_ENV environment variable. This
- * method also optionally applies special testing configuration values to the
- * retrieved configuration if the APP_TESTING variable is set to 'true'.
+ * Gets the current application configuration from configuration.json
  * 
  * @return {Object} The current configuration.
  */
 function get() {
-    if (process.env.APP_ENV === 'development') {
-        return Object.assign({}, dev, process.env.APP_TESTING === 'true' ? test : {});
-    } else if (process.env.APP_ENV === 'production') {
-        return Object.assign({}, prod, process.env.APP_TESTING === 'true' ? test : {});
-    } else {
-        return Object.assign({}, dev, process.env.APP_TESTING === 'true' ? test : {});
-    }
+    return JSON.parse(JSON.stringify(config));
 }
 
 module.exports = {
--- config/regexes-dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    token: /^.{1,16}$/,
-    phoneNumber: /^(\+\d+)?\s?(\(?\d{3}\)?)?(-|\s|\.)?\d{3}(-|\s|\.)?\d{4}$/,
-    password: /^.{8,}$/
-};
